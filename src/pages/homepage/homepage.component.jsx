dde1370df8793c8210f7dd1deb084f15aa527dbb
Yangba Tongbram <yangba.tongbram@eurocorp.co.nz>
2020-04-13 11:05:36 +1200

    added header component and shop page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,8 +5,10 @@ import './homepage.styles.scss'
 import Directory from "../../components/directory/directory.component";
 
 
+
 const HomePage = () =>(
     <div className='homepage'>
+        
         <Directory></Directory>
     </div>
 
