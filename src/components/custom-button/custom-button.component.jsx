c7f47685a177c7adb4a6122c0e77ea6f8cdfe773
Yangba Tongbram <yangba.tongbram@eurocorp.co.nz>
2020-04-19 11:22:24 +1200

    updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import "./custom-button.styles.scss";
 
 
-const CutomButton = ({children, isGoogleSignIn, ...otherProps}) =>(
+const CutomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) =>(
 
-    <button className = {`${isGoogleSignIn ? 'google-sign-in':''} custom-button`} {...otherProps}>
+    <button className = {`${inverted ? 'inverted':''} ${isGoogleSignIn ? 'google-sign-in':''} custom-button`} {...otherProps}>
         {children}
     </button>
 )
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,6 +2,7 @@ import { UserActionTypes } from "./user.types";
 
 const INITIAL_STATE={
     currentUser: null
+   
 }
 
 const userReducer =(state = INITIAL_STATE, action) =>{
@@ -12,6 +13,8 @@ const userReducer =(state = INITIAL_STATE, action) =>{
                 ...state,
                 currentUser: action.payload   
             }
+        
+        
 
         default:
             return state;
