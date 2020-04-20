9138309cd17c34716122b0f2b4cd4cb4823f41cc
Yangba Tongbram <yangba.tongbram@eurocorp.co.nz>
2020-04-20 20:05:18 +1200

    added checkout page, checkout item component and updated cart to use selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,6 +9,11 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+
+    cart => cart.hidden
+)
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
 
@@ -20,4 +25,14 @@ export const selectCartItemsCount = createSelector(
         )
 )
 
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+
+    cartItems =>
+        cartItems.reduce(
+            (accumalatedQuantity, cartItem)=>
+            accumalatedQuantity + cartItem.quantity * cartItem.price,
+         0
+        )
+)
 
